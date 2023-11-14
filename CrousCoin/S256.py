cb738091a4ab0d041602b9a2992933ed3f759463
Brandon3118 <brandon.afonso3118@gmail.com>
2023-11-14 23:18:32 +0100

    update SHA256

--- CrousCoin/S256.py
+++ CrousCoin/S256.py
@@ -1,5 +1,10 @@
 from FieldElement import FieldElement
 from Point import Point
+import hashlib
+import hmac
+from random import randint
+import unittest
+
 P=2**256 - 2**32 - 977
 N=0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
 
@@ -13,13 +18,12 @@ class S256Field(FieldElement):
 
 A,B = 0,7
 class S256Point(Point):
-    def __init__(self, x,y ,a=None, b=None):
-        a,b = S256Field(A),S256Field(B)
+    def __init__(self, x, y, a=None, b=None):
+        a, b = S256Field(A), S256Field(B)
         if type(x) == int:
-            super().__init__(S256Field(x),S256Field(y),a,b)
+            super().__init__(x=S256Field(x), y=S256Field(y), a=a, b=b)
         else:
-            super().__init__(x,y,a,b)
-            
+            super().__init__(x=x, y=y, a=a, b=b)
     def __repr__(self):
         if self.x is None:
             return '256Point(infinity)'
@@ -29,26 +33,78 @@ class S256Point(Point):
         coef = coefficient%N  #avec NG = 0
         return super().__rmul__(coef)
 
+    def verify(self, z, sig):
+        s_inv = pow(sig.s, N-2, N)
+        u = z*s_inv % N
+        v = sig.r*s_inv % N
+        total = u*G+v*self
+        return total.x.num == sig.r
+
 G = S256Point(
  0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
  0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8)
 
-print(N*G) #--> infinity exemple1
+class Signature:
+    def __init__(self, r, s):
+        self.r = r
+        self.s = s
+    def __repr__(self):
+        return 'Signature({:x}, {:x})'.format(self.r, self.s)
 
-#P=eG  P->Public key e->Private key
-z = 0xbc62d4b80d9e36da29c16c5d4d9f11731f36052c72401a76c23c0fb5a9b74423
-r = 0x37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6
-s = 0x8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec
-px = 0x04519fac3d910ca7e7138f7013706f619fa8f033e6ec6e09370ea38cee6a7574
-py = 0x82b51eab8c27c66e26c858a079bcdf4f1ada34cec420cafc7eac1a42216fb6c4
-point = S256Point(px, py)
-s_inv = pow(s, N-2, N)
 
-u = z * s_inv % N
-v = r * s_inv % N
+"""    pb hash256 
+a_string = 'my secret'
+e = int(sha256(a_string.encode('utf-8')).hexdigest(), 16)
+e = e.from_bytes(e, 'big')
+a_string = 'my message'
+z = int(sha256(a_string.encode('utf-8')).hexdigest(),16)
+z = z.from_bytes(z, 'big')
+k = 1234567890
+r = (k*G).x.num
+k_inv = pow(k, N - 2, N)
+s = (z + r * e) * k_inv % N
+point = e * G
+print(point)
+print(hex(z))
+print(hex(r))
+print(hex(s))
+#P=eG  P->Public key e->Private key"""
 
-print((u*G + v*point).x.num == r)
+class PrivateKey:
+    def __init__(self, secret):
+        self.secret = secret
+        self.point = secret*G
+    def hex(self):
+        return '{:x}'.format(self.secret).zfill(64)
 
+    def sign(self, z):
+        k = self.deterministic_k(z)
+        r = (k*G).x.num
+        k_inv = pow(k, N-2,N)
+        s = (z+r*self.secret)*k_inv%N
+        if s > N/2:
+            s = N-s
+        return Signature(r, s)
+
+    def deterministic_k(self, z):
+        k = b'\x00' * 32
+        v = b'\x01' * 32
+        if z > N:
+            z -= N
+        z_bytes = z.to_bytes(32, 'big')
+        secret_bytes = self.secret.to_bytes(32, 'big')
+        s256 = hashlib.sha256
+        k = hmac.new(k, v + b'\x00' + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        k = hmac.new(k, v + b'\x01' + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        while True:
+            v = hmac.new(k, v, s256).digest()
+            candidate = int.from_bytes(v, 'big')
+            if candidate >= 1 and candidate < N:
+                return candidate
+            k = hmac.new(k, v + b'\x00', s256).digest()
+            v = hmac.new(k, v, s256).digest()
 
 """gx = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
 gy = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
@@ -68,3 +124,48 @@ seven = FieldElement(7, p)
 zero = FieldElement(0, p)
 G = Point(x, y, zero, seven)
 print(n*G)"""
+class S256Test(unittest.TestCase):
+
+    def test_order(self):
+        point = N * G
+        self.assertIsNone(point.x)
+
+    def test_pubpoint(self):
+        # write a test that tests the public point for the following
+        points = (
+            # secret, x, y
+            (7, 0x5cbdf0646e5db4eaa398f365f2ea7a0e3d419b7e0330e39ce92bddedcac4f9bc, 0x6aebca40ba255960a3178d6d861a54dba813d0b813fde7b5a5082628087264da),
+            (1485, 0xc982196a7466fbbbb0e27a940b6af926c1a74d5ad07128c82824a11b5398afda, 0x7a91f9eae64438afb9ce6448a1c133db2d8fb9254e4546b6f001637d50901f55),
+            (2**128, 0x8f68b9d2f63b5f339239c1ad981f162ee88c5678723ea3351b7b444c9ec4c0da, 0x662a9f2dba063986de1d90c2b6be215dbbea2cfe95510bfdf23cbf79501fff82),
+            (2**240 + 2**31, 0x9577ff57c8234558f293df502ca4f09cbc65a6572c842b39b366f21717945116, 0x10b49c67fa9365ad7b90dab070be339a1daf9052373ec30ffae4f72d5e66d053),
+        )
+
+        # iterate over points
+        for secret, x, y in points:
+            # initialize the secp256k1 point (S256Point)
+            point = S256Point(x, y)
+            # check that the secret*G is the same as the point
+            self.assertEqual(secret * G, point)
+
+    def test_verify(self):
+        point = S256Point(
+            0x887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e4da568744d06c,
+            0x61de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34)
+        z = 0xec208baa0fc1c19f708a9ca96fdeff3ac3f230bb4a7ba4aede4942ad003c0f60
+        r = 0xac8d1c87e51d0d441be8b3dd5b05c8795b48875dffe00b7ffcfac23010d3a395
+        s = 0x68342ceff8935ededd102dd876ffd6ba72d6a427a3edb13d26eb0781cb423c4
+        self.assertTrue(point.verify(z, Signature(r, s)))
+        z = 0x7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d
+        r = 0xeff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c
+        s = 0xc7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6
+        self.assertTrue(point.verify(z, Signature(r, s)))
+
+class PrivateKeyTest(unittest.TestCase):
+
+    def test_sign(self):
+        pk = PrivateKey(randint(0, N))
+        z = randint(0, 2**256)
+        sig = pk.sign(z)
+        self.assertTrue(pk.point.verify(z, sig))
+if __name__ == '__main__':
+    unittest.main()
