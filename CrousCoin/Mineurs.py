7ac66bb6d4d669ed8366ec37e5ca635a3602a4b8
Hog Rider <125391799+HogRiderCaptain@users.noreply.github.com>
2024-01-06 20:00:19 +0100

    Update Mineurs.py

--- CrousCoin/Mineurs.py
+++ CrousCoin/Mineurs.py
@@ -16,18 +16,24 @@ class Mineur:
     def __ne__(self,other):
         return not(self.__eq__(other))
     
-    def create_block(self,transa):
+   def create_block(self, transa, bc):
         t = time()
-        block = Block(transa)
-        sleep(randint(0,5))
-        t-= time()
-        return block,-t
-    
-    def get_rewards(self,fastest):
-        if self.__eq__(fastest):
+        Block = block(transa)
+        self.minage_bloc(Block, bc)
+        sleep(randint(0, 5))
+        t -= time()
+        return Block, -t
+
+    def get_rewards(self, fastest):
+        if self.eq(fastest):
             self.wallet += 10
             print("Mineur " + self.name + " a été le plus performant, il remporte 10MC")
         else:
             print("Mineur " + self.name + " a été trop lent")
+
+    def minage_bloc(self, bloc, bc):
+        if bc.size > 0:
+            bloc.prev = bc.chain[-1].hash
+        bloc.pW = bloc.proofOfWork()
     
     
