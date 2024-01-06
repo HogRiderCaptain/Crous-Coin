f02d9915921b5fe231eaa3970d245fc01a1a84e1
Hog Rider <125391799+HogRiderCaptain@users.noreply.github.com>
2024-01-06 19:59:38 +0100

    Update simulation.py

--- CrousCoin/simulation.py
+++ CrousCoin/simulation.py
@@ -2,7 +2,7 @@ from transaction import transaction
 from Personne import Personne
 from blockChain import *
 from random import choice, randint
-from Mineurs import Mineur
+from mineurs import Mineur
 
 Brand = Personne("Brand", 1000, "jaimelecaca")
 Sergio = Personne("Sergio", 1000, "jesuispd")
@@ -10,61 +10,55 @@ Youss = Personne("Mugsy99", 1000, "fumernetuepas")
 personne = Personne("Levy", 1000, "jsp")
 Ali = Personne("Goat", 1000, "goatultime.")
 
-transactions = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
-n=0
+mineur = Mineur("Mineur", 0)
+
+#transactions = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+n = 0
 goats = [Brand, Sergio, Youss, personne, Ali]
 
-for i in range(len(transactions)):
-    for j in range(5):
+mineurs = [Mineur("Rtx4080", 0), Mineur("Rtx4090", 0), Mineur("Rtx5090", 0), Mineur("Rtx4070", 0)]
+
+
+numT = 0
+transactions = []
+bc = blockChain()
+
+
+
+def print_resultats(fastest):
+    for mineur in mineurs:
+        mineur.get_rewards(fastest)
+
+def minage(bloc):
+    liste_tps = []
+    bloc_mine = bloc
+    for mineur in mineurs:
+        bloc_mine, tps = mineur.create_block(bloc.transa, bc)
+        liste_tps.append(tps)
+
+    print("Block " + bloc_mine.hash)
+    print("----------------------")
+    print_resultats(mineurs[liste_tps.index(min(liste_tps))])
+    print("----------------------")
+    print("")
+
+    return bloc_mine
+
+for i in range(4):
+    while len(transactions) < 5:
         input = choice(goats)
         output = choice(goats)
-        while output==input:
+        while output == input:
             output = choice(goats)
-        amount = randint(0,1000)
-        transactions[i][j] = transaction(input, output, amount, n)
-        n+=1
+        amount = randint(0, 1000)
+        while input.wallet < amount:
+            amount = randint(0, 1000)
+        transactions.append(transaction(input, output, amount, numT))
+        numT+=1
 
+    bloc = block(transactions)
+    bc.add(minage(bloc))
 
+    transactions = []
 
-mineurs = [Mineur("Rtx4080",0),Mineur("Rtx4090",0),Mineur("Rtx4060ti",0),Mineur("Rtx4070",0),Mineur("Rtx4060",0)]
-list_time = [0 for i in range(len(mineurs))]
-
-block = None
-for _ in range(len(mineurs)):
-    block,list_time[_] = mineurs[_].create_block(transactions[0])
-print(block.hash)   
-min = list_time[0]
-fastest_block = mineurs[0]
-for _ in range(1,len(list_time)):
-    if list_time[_] < min:
-        min = list_time[_]
-        fastest_block = mineurs[_]
-
-for _ in mineurs:
-    _.get_rewards(fastest_block)
-
-bc = BlockChain(block)
-
-for i in transactions[1:]:
-    for _ in range(len(mineurs)):
-        block,list_time[_] = mineurs[_].create_block(i)
-    print(block.hash)
-    min = list_time[0]
-    fastest_block = mineurs[0]
-    for _ in range(1,len(list_time)):
-        if list_time[_] < min:
-            min = list_time[_]
-            fastest_block = mineurs[_]
-
-    for _ in mineurs:
-        _.get_rewards(fastest_block)
-    
-    bc.add(block)
-        
-
-
-#print(Brand.wallet, Ali.wallet, Youss.wallet, personne.wallet, Sergio.wallet)
-for _ in mineurs:
-    print(_.name + ": " + str(_.wallet) + "MC\n")
-    
 print(bc)
