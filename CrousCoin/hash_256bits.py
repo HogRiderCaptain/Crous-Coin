ddd218b93faf9160079f6f2f4c15bcad7e9170bf
Brandon3118 <brandon.afonso3118@gmail.com>
2024-01-08 00:31:12 +0100

    add comment Hash256

--- CrousCoin/hash_256bits.py
+++ CrousCoin/hash_256bits.py
@@ -2,6 +2,6 @@ import hashlib
 
 def hash256(message_encode):
     """Fonction pour créer le hashage d'un message encode passé en paramètre. Ici on hash 2 fois pour plus de sécurité.
-    L'encodage choisi est le big indian d'ou le big comme 2ème paramètre de int.from_bytes"""
+    L'encodage choisi est le big indian d'ou le big comme 2ème paramètre de int.from_bytes."""
     return int.from_bytes(hashlib.sha256(hashlib.sha256(message_encode).digest()).digest(), 'big')
 
