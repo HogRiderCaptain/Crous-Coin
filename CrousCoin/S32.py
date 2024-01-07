615838cfd65a60bdcce580d4656133f580ef1af1
Brandon3118 <brandon.afonso3118@gmail.com>
2024-01-07 02:45:17 +0100

    essai commit w/o bug

--- CrousCoin/S32.py
+++ CrousCoin/S32.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from FieldElement import FieldElement
 from Point import Point
 
@@ -40,47 +39,4 @@ class S32Point(Point):
         return V.x == signature.r
 
 G = S32Point(1, 3)
-=======
-from FieldElement import FieldElement
-from Point import Point
-
-P=4294956461
-N=715826077
-A,B = 0,8
-
-class S32Field(FieldElement):
-        
-    def __init__(self,num,prime=None):
-        super().__init__(num,prime=P)
-        
-    def __repr__(self):
-        return '{:x}'.format(self.num).zfill(8) 
-
-
-class S32Point(Point):
-    def __init__(self, x, y, a=None, b=None):
-        a,b=S32Field(A),S32Field(B)
-        if type(x) == int:
-            super().__init__(x=S32Field(x), y=S32Field(y), a=a, b=b)
-        else:
-            super().__init__(x=x, y=y, a=a, b=b)
-            
-    def __repr__(self):
-        if self.x is None:
-            return '32Point(infinity)'
-        return '32Point({},{})_{}_{}'.format(self.x, self.y, A, B)
-
-    def __rmul__(self, coefficient):
-        coef = coefficient%N          #avec NG = 0
-        return super().__rmul__(coef)
-
-    def verify(self, z, signature):
-        s_inv = pow(signature.s, N-2, N)
-        u = s_inv * z % N
-        v = s_inv * signature.r.num % N
-        V = u*G + v*self
-        return V.x == signature.r
-
-G = S32Point(1, 3)
->>>>>>> c43a8e61742a36c5474e07e7851ba98b2d8ed839
 #print((N-1)*G)
