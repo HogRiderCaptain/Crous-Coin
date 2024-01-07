772ca74cd364bf7c849e29304ec29677717483d2
Brandon3118 <brandon.afonso3118@gmail.com>
2024-01-08 00:17:18 +0100

    add comment Signature

--- CrousCoin/Signature.py
+++ CrousCoin/Signature.py
@@ -1,7 +1,11 @@
 class Signature:
     def __init__(self, r, s):
+        """Initialisation de la class avec le produit du nombre random et le générateur G 
+        (ici r) et s la signature."""
         self.r = r
         self.s = s
         
     def __repr__(self):
+        """Fonction overwrite pour remplacer l'affichage de l'objet Signature seulement 
+        l'hexadécimal de la valeur entière de la signature."""
         return 'Signature : {}'.format(hex(self.s))
